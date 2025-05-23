ebe8eeb1c3d48974cae3d76843769c2e08261a57
Mindp1 <manandp2@illinois.edu>
2025-05-23 15:47:47 -0500

    Track total wh along with delta to be able to accurately track usage.

--- database.go
+++ database.go
@@ -22,17 +22,20 @@ func UpdateTimeScaleDb(vitals *Vitals, price *HourlyPrice) {
 
 	// Get the previous wh reading
 	var prevWh float64
-	err = conn.QueryRow(ctx, "SELECT wh FROM charging_stats ORDER BY time DESC LIMIT 1").Scan(&prevWh)
+	err = conn.QueryRow(ctx, "SELECT total_wh FROM charging_stats ORDER BY time DESC LIMIT 1").Scan(&prevWh)
 	if err != nil {
 		fmt.Printf("SELECT Error: %s\n", err)
 		return
 	}
-
-	whInLastHour := max(vitals.SessionEnergyWh-prevWh, 0)
-
+	var whInLastHour float64
+	if vitals.SessionEnergyWh-prevWh < 0 {
+		whInLastHour = vitals.SessionEnergyWh
+	} else {
+		whInLastHour = vitals.SessionEnergyWh - prevWh
+	}
 	// Insert the new wh reading
-	insertQuery := `INSERT INTO charging_stats (time, wh, price, cost) VALUES ($1, $2, $3, $4)`
-	_, err = conn.Exec(ctx, insertQuery, time.Now(), whInLastHour, price.Price, (whInLastHour/1000)*(price.Price/100))
+	insertQuery := `INSERT INTO charging_stats (time, total_wh, wh_difference, price, cost) VALUES ($1, $2, $3, $4, $5)`
+	_, err = conn.Exec(ctx, insertQuery, time.Now(), vitals.SessionEnergyWh, whInLastHour, price.Price, (whInLastHour/1000)*(price.Price/100))
 	if err != nil {
 		fmt.Printf("INSERT Error: %s\n", err)
 	}
